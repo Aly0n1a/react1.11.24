7910e465b15eb0b6849a2a9fe7e357a3b7c726ad
Чмирёва Алёна <meloman.ytxd@gmail.com>
2024-11-01 19:01:15 +0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.jsx'
+import Header from './components/Header/Header.jsx'
+import Hero from './components/Hero/Hero.jsx'
+const App = () => {
+  return (
+    <>
+    <Header/>
+    <Hero/>
+    </>
+  )
+}
+
+export default App
